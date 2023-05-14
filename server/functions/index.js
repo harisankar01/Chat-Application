a6ddc544202c3379eafb4c4735d92be050b74fa8
hari <hsankar@dinify.io>
2023-05-14 13:21:30 +0530

    changes for firebase hosting

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,3 +1,15 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const { onRequest } = require("firebase-functions/v2/https");
+const functions = require("firebase-functions");
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -49,3 +61,12 @@ io.on("connection", (socket) => {
     }
   });
 });
+
+exports.app = functions.https.onRequest(app);
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
